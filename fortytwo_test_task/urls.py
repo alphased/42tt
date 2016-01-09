195c0fda56f5c24bdec09ccce8f08f5f428d1dfc
alphased <alphasedout@gmail.com>
2016-01-09 19:21:27 +0200

    ticket:1 Create Django template with hard-coded data

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,13 +1,19 @@
 from django.conf.urls import patterns, include, url
 
+from django.views.generic.base import TemplateView
+
 from django.contrib import admin
 admin.autodiscover()
+
 
 urlpatterns = patterns(
     '',
     # Examples:
     # url(r'^$', 'fortytwo_test_task.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+
+    # from django.views.generic.base import TemplateView
+    url(r'^$', TemplateView.as_view(template_name='hello/index.html')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
