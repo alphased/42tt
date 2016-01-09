1483834c0a1811761cd41ab28de888e4092710a7
alphased <alphasedout@gmail.com>
2016-01-09 20:26:57 +0200

    ticket:1 Create test for the view with hard-coded template

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,9 +1,20 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.http import HttpRequest
+from .views import home
 
-# Create your tests here.
 
+class HomePageTests(TestCase):
 
-class SomeTests(TestCase):
-    def test_math(self):
-        "put docstrings in your tests"
-        assert(2 + 2 == 5)
+    def test_hardcoded_data(self):
+        '''Checking presence of hardcoded data
+        '''
+        client = Client()
+        response = client.get('/')
+        self.assertContains(response, '42 Coffee Cups Test Assignment')
+
+    def test_homepage_view(self):
+        '''Checking presence of hardcoded data in the home view
+        '''
+        request = HttpRequest()
+        response = home(request)
+        self.assertContains(response, '42 Coffee Cups Test Assignment')
--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -6,14 +6,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 
-urlpatterns = patterns(
-    '',
-    # Examples:
-    # url(r'^$', 'fortytwo_test_task.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    # from django.views.generic.base import TemplateView
+urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='hello/index.html')),
-
     url(r'^admin/', include(admin.site.urls)),
 )
