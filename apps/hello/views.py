797daf64d5f1190f9a3a3ba7a724a06fd05a4f0a
alphased <alphasedout@gmail.com>
2016-01-09 20:53:11 +0200

    ticket:1 Create view

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    '''
+    from django.views.generic.base import TemplateView
+    url(r'^$', TemplateView.as_view(template_name='hello/index.html')),
+    '''
+    return render(request, 'hello/index.html')
--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls import patterns, include, url
 
-from django.views.generic.base import TemplateView
-
 from django.contrib import admin
 admin.autodiscover()
 
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name='hello/index.html')),
+    url(r'^$', 'hello.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
