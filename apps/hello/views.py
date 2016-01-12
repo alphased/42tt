032793856b21033424d0fdeeb7428540a65e189d
alphased <alphasedout@gmail.com>
2016-01-12 17:11:20 +0200

    ticket:1 Amend the view. All test should pass

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
 from .models import User
 
 import logging
 logger = logging.getLogger(__name__)
+
+DEFAULT_USER_PROFILE = {
+    "username": "admin",
+    "first_name": "Andrii",
+    "last_name": "Ilika",
+    "birthday": "1980-11-14",
+    "bio": "Parchment then, is that your holy well,\r\n"
+           "from which drink always slakes your thirst?\r\n"
+           "You'll never truly be refreshed until\r\n"
+           "It pours itself from your own soul, first.",
+    "email": "alphasedout@gmail.com",
+    "contact_jabber": "alphased@khavr.com",
+    "contact_skype": "ai22-uanic",
+    "othercontacts": "Enough is enough",
+}
 
 
 def home(request):
@@ -12,7 +28,7 @@ def home(request):
     is always for user 'admin'
     '''
     logger.debug('request user: %s' % request.user)
-    profile = None
+    profile = User(**DEFAULT_USER_PROFILE)
     try:
         profile = User.objects.get(username='admin')
     except ObjectDoesNotExist as e:
