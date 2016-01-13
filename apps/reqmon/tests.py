46dc2c80f3c75fbbf87ff62eb4195aed9ad8bce4
alphased <alphasedout@gmail.com>
2016-01-13 22:56:24 +0200

    ticket:3 Tests for pages

--- apps/reqmon/tests.py
+++ apps/reqmon/tests.py
@@ -1,3 +1,18 @@
-# no code here from django.test import TestCase
+from django.test import Client, TestCase
+from django.core.urlresolvers import reverse
+import beautifulsoupselect as bss
 
-# Create your tests here.
+
+class ResuestsPageTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.link_home = reverse('home')
+        self.link_requests = reverse('requests')
+
+    def test_home_link(self):
+        '''Page contains the link to the 'home' page
+        '''
+        response = self.client.get(self.link_requests)
+        soup = bss.BeautifulSoupSelect(response.content)
+        self.assertTrue(soup('a[href="%s"]' % self.link_home))
