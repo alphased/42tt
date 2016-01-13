87ccda8784c158546620f52a735d5642a9905c5f
alphased <alphasedout@gmail.com>
2016-01-13 23:19:21 +0200

    ticket:3 Create templates

--- apps/reqmon/views.py
+++ apps/reqmon/views.py
@@ -1,3 +1,5 @@
-# no code here from django.shortcuts import render
+from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'reqmon/index.html')
