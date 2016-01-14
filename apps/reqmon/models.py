74061a22c9d8fec001a84e38b9f8463be8f0d8e8
alphased <alphasedout@gmail.com>
2016-01-14 19:32:05 +0200

    ticket:3 Implement model`

--- apps/reqmon/models.py
+++ apps/reqmon/models.py
@@ -1,3 +1,10 @@
-# no code here from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class Requests(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True)
+    method = models.CharField(max_length=8)
+    path = models.CharField(max_length=256)
+
+    def __unicode__(self):
+        return u'%s %s' % (self.method, self.path)
