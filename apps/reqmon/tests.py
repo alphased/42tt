ebc16b302020fce57319e9998c44c935b53c4579
alphased <alphasedout@gmail.com>
2016-01-19 23:00:55 +0200

    ticket:13 Create tests

--- apps/reqmon/tests.py
+++ apps/reqmon/tests.py
@@ -20,6 +20,8 @@ class RequestsMiddlewareTests(TestCase):
 
     def setUp(self):
         self.client = Client()
+        self.link_home = reverse('home')
+        self.link_admin = reverse('admin:index')
         self.link_requests = reverse('requests')
 
     def test_middleware_got_requests(self):
@@ -28,12 +30,31 @@ class RequestsMiddlewareTests(TestCase):
         self.client.get(self.link_requests)
         self.assertEquals(self.link_requests, Requests.objects.get(pk=1).path)
 
+    def test_middleware_priority0_home(self):
+        '''Middleware set priority 0 for request to home
+        '''
+        self.client.get(self.link_home)
+        self.assertEquals(0, Requests.objects.get(pk=1).priority)
+
+    def test_middleware_priority1_admin(self):
+        '''Middleware set priority 1 for request to admin
+        '''
+        self.client.get(self.link_admin)
+        self.assertEquals(1, Requests.objects.get(pk=1).priority)
+
+    def test_middleware_priority1_post(self):
+        '''Middleware set priority 1 for POST requests
+        '''
+        self.client.post(self.link_requests)
+        self.assertEquals(1, Requests.objects.get(pk=1).priority)
+
 
 class RequestsPageTests(TestCase):
 
     def setUp(self):
         self.client = Client()
         self.link_home = reverse('home')
+        self.link_admin = reverse('admin:index')
         self.link_requests = reverse('requests')
 
     def test_home_link(self):
@@ -43,8 +64,9 @@ class RequestsPageTests(TestCase):
         soup = bss.BeautifulSoupSelect(response.content)
         self.assertTrue(soup('a[href="%s"]' % self.link_home))
 
-    def test_amount_ordering(self):
+    def test_amount_ordering_reverse(self):
         '''View contains no more than last 10 requests and has the proper ordering
+           (all params by default: reverse=1, priority=0)
         '''
         for _ in range(10):
             self.client.head(self.link_home)
@@ -52,6 +74,22 @@ class RequestsPageTests(TestCase):
         soup = bss.BeautifulSoupSelect(response.content)
         self.assertEquals(10, len(soup('ul#requests li')))
         self.assertIn(self.link_requests, soup('ul#requests li')[0].text)
+
+    def test_amount_ordering_chronological(self):
+        '''View contains no more than last 10 requests and has the proper ordering
+           (all params by default: reverse=0, priority=1)
+        '''
+        for _ in range(10):
+            self.client.post(self.link_requests)
+        response = self.client.get(self.link_admin)
+        response = self.client.get(self.link_requests,
+                                   {'priority': 1, 'reverse': 0})
+        soup = bss.BeautifulSoupSelect(response.content)
+        self.assertEquals(10, len(soup('ul#requests li')))
+        self.assertIn(self.link_requests,
+                      soup('ul#requests li')[8].text)
+        self.assertIn(self.link_admin,
+                      soup('ul#requests li')[9].text)
 
     def test_form_contains(self):
         '''view form contains latest request_id
@@ -74,9 +112,10 @@ class RequestsUpdatesTests(TestCase):
         '''Ajax endpoint returns results for zero, one and multiple requests
         '''
 
-        def do_request(last):
+        def do_request(last, priority=0):
             self.client.get(self.link_requests)
-            return self.client.get(self.link_requests_updates, {'last': last},
+            return self.client.get(self.link_requests_updates,
+                                   {'last': last, 'priority': priority},
                                    HTTP_X_REQUESTED_WITH='XMLHttpRequest',
                                    follow=False)
 
@@ -120,3 +159,18 @@ class RequestsUpdatesTests(TestCase):
         self.assertEquals(200, response.status_code)
         self.assertEquals(7, len(result['requests']))
         self.assertEquals(7, result['latest'])
+
+        # Got one request with priority 1
+        self.client.post(self.link_requests_updates)
+        response = do_request(0, 1)
+        result = json.loads(response.content)
+        self.assertEquals(200, response.status_code)
+        self.assertEquals(1, len(result['requests']))
+        self.assertEquals(8, result['latest'])
+
+        # Here goes second
+        self.client.post(self.link_requests_updates)
+        response = do_request(0, 1)
+        result = json.loads(response.content)
+        self.assertEquals(2, len(result['requests']))
+        self.assertEquals(10, result['latest'])
