36558ef9eec1192f2fe450467bb2087093d642f2
alphased <alphasedout@gmail.com>
2016-01-20 10:48:28 +0200

    ticket:13 Implement middleware

--- apps/reqmon/middleware.py
+++ apps/reqmon/middleware.py
@@ -2,9 +2,22 @@ from reqmon.models import Requests
 from django.core.urlresolvers import reverse
 
 
+SKIP_PATH = [reverse('requests_updates'), ]
+PRI1_PATH = [reverse('admin:index'), ]
+
+
 class RequestsMiddleware():
 
     def process_request(self, request):
-        if reverse('requests_updates') != request.path:
-            Requests.objects.create(path=request.path, method=request.method)
-        return None
+        if request.path in SKIP_PATH:
+            return None
+
+        request_kwargs = {}
+        if request.method == 'POST':
+            request_kwargs['priority'] = 1
+        elif request.path in PRI1_PATH:
+            request_kwargs['priority'] = 1
+
+        Requests.objects.create(path=request.path,
+                                method=request.method,
+                                **request_kwargs)
