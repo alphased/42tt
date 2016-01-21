1417b638e06f7cea56e1d7d3b793fab875ff97e8
alphased <alphasedout@gmail.com>
2016-01-21 16:15:33 +0200

    ticket:8 Create tests for edit_link tag

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,6 +1,8 @@
 from django.test import Client, TestCase
 from django.core.urlresolvers import reverse
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from django.template import Template, Context
 import beautifulsoupselect as bss
 
 
@@ -59,3 +61,26 @@ class HomePageTests(TestCase):
         response = self.client.get(self.link_home)
         soup = bss.BeautifulSoupSelect(response.content)
         self.assertTrue(soup('a[href="%s"]' % self.link_requests))
+
+
+class EditLinkTagTests(TestCase):
+
+    def setUp(self):
+        self.link_home = reverse('home')
+        self.link_admin_user = reverse('admin:hello_user_change', args=(1,))
+        self.link_auth_group = reverse('admin:auth_group_change', args=(1,))
+        self.template = Template("{% load edit_link %}{% edit_link object %}")
+
+    def test_edit_link_admin(self):
+        '''Page contains the link to the initial user in admin
+        '''
+        response = self.client.get(self.link_home)
+        soup = bss.BeautifulSoupSelect(response.content)
+        self.assertTrue(soup('a[href="%s"]' % self.link_admin_user))
+
+    def test_edit_link_auth_group(self):
+        group = Group.objects.create(name='linkme')
+        context = Context({'object': group})
+        rendered = self.template.render(context)
+        self.assertIn(self.link_auth_group, rendered)
+
