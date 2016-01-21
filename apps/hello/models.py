fe461bcc4fd901c939f7f26c6a5dbd48e77316ef
alphased <alphasedout@gmail.com>
2016-01-21 15:29:06 +0200

    ticket:8 Create a model. Magrate on staging

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.contenttypes.models import ContentType
 
 
 class User(AbstractUser):
@@ -8,3 +9,17 @@ class User(AbstractUser):
     contact_skype = models.CharField(max_length=30, blank=True)
     othercontacts = models.TextField(blank=True)
     bio = models.TextField(blank=True)
+
+
+class Event(models.Model):
+
+    action_time = models.DateTimeField(auto_now=True)
+    content_type = models.ForeignKey(ContentType, blank=True, null=True)
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.PositiveSmallIntegerField()
+
+    class Meta:
+        verbose_name = "event"
+        verbose_name_plural = "events"
+        ordering = ('-action_time',)
